b48244e996e0af2ccf89d907e7b6e1818daa33c3
Maite <miemail@gmail.com>
2018-05-25 10:10:40 +0200

    Subido el backend del proyecto

---
+++ Proyecto/Back-end/src/application/RESTApplication.java
@@ -0,0 +1,14 @@
+package application;
+
+import javax.ws.rs.ApplicationPath;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+@ApplicationPath("services")
+public class RESTApplication extends ResourceConfig {
+
+  public RESTApplication () {
+    packages("ws.services");
+  }
+
+}
