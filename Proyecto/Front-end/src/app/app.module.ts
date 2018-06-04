de999739b473bcba03ddd6e60660eecdd112f5d4
Maite <miemail@gmail.com>
2018-06-04 13:03:01 +0200

    Ahora se navega al componente de login introduciendo la ruta /login

--- Proyecto/Front-end/src/app/app.module.ts
+++ Proyecto/Front-end/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FrontEndComponent } from './components/front-end/front-end.component';
 import { ListadoComponent } from './components/listado/listado.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { AppRoutingModule } from './app.routing';
 
 @NgModule({
   declarations: [
     AppComponent,
     FrontEndComponent,
-    ListadoComponent
+    ListadoComponent,
+    LoginPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
