b5043cee8195297bfaef291c70bd67174c5e4815
DESKTOP-LB0VDF8\maite <maitejr_95@hotmail.com>
2018-06-06 23:07:24 +0200

    Ahora reenvía al login si no está identificado. Falta cargar el navbar mostrando otras rutas si se está identificado (probar con un endpoint en backend que se llame siempre que se cargue navbar)

--- Proyecto/Front-end/src/app/app.component.ts
+++ Proyecto/Front-end/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  private isLogged : boolean;
+  //EndPoint que diga si estoy loggeada
 }
