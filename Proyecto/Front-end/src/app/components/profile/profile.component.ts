0f8b4a4678d1397741daf05758adf06ef239292b
Maite <miemail@gmail.com>
2018-06-07 10:13:14 +0200

    Ya se ve correctamente el perfil de usuario

--- Proyecto/Front-end/src/app/components/profile/profile.component.ts
+++ Proyecto/Front-end/src/app/components/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../../models/usuario';
+import { UserService } from '../../services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  private isLogged : boolean;
+  private loggedUser : Usuario;
 
-  constructor() { }
+  constructor(private userService : UserService,
+    private router : Router) { }
 
   ngOnInit() {
+    let cookies : String[] = document.cookie.split(";");
+    let token : string = "";
+    this.isLogged = false;
+    for(let cookie of cookies){
+      if(cookie.includes("pepe_login")){
+        token = cookie.substring(11);
+        break;
+      }
+    }
+    console.log("tras break", token);
+    token = !token || token.trim().length == 0? "" : token.substring(36, token.length-36);
+    if(token == ""){      
+      this.router.navigate(['/']);
+    }
+    try{
+      var id : number= Number.parseInt(token);
+      this.userService.getUser(id).subscribe(user => {
+        this.loggedUser = user;
+        this.isLogged = true;
+      });
+    } catch {
+      this.isLogged = false; 
+    }
+    
   }
 
 }
